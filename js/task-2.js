7c2c1eeeaecc0722509cc9150916be129f8bf946
Unik7722 <129215970+Unik7722@users.noreply.github.com>
2024-10-18 17:26:40 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -10,12 +10,10 @@ class Storage{
         this.#items.push(newItem);
     }
     removeItem(itemToRemove){
-        const index = this.#items.indexOf(itemToRemove);
-        if (index !== -1){
-            this.#items.splice(index, 1);
-        }     
+      this.#items = this.#items.filter(item => item !== itemToRemove); 
+    }     
     }
-}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
