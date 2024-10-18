8bce46a72ab514db48c87d1a6930d3c1f6b3c761
Unik7722 <129215970+Unik7722@users.noreply.github.com>
2024-10-18 15:44:43 +0300

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -5,28 +5,58 @@
     discount: 0.1,
     orders: ["Burger", "Pizza", "Salad"],
     // Change code below this line
-    this.getBalance() {
-      return balance;
+    getBalance() {
+      return this.balance;
     },
-    this.getDiscount() {
-      return discount;
+    getDiscount() {
+      return this.discount;
     },
-    this.setDiscount(value) {
-      discount = value;
+    setDiscount(value) {
+      this.discount = value;           
     },
-    this.getOrders() {
-      return orders;
+    getOrders() {
+      return this.orders;
     },
-    this.addOrder(cost, order) {
-      balance -= cost - cost * discount;
-      orders.push(order);
+    addOrder(cost, order) {
+      this.balance -= cost - cost * this.discount;
+      this.orders.push(order);
     },
     // Change code above this line
   };
-  
+
   customer.setDiscount(0.15);
   console.log(customer.getDiscount()); // 0.15
   customer.addOrder(5000, "Steak");
   console.log(customer.getBalance()); // 19750
   console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]  
 
+  // const customer = {
+  //   username: "Mango",
+  //   balance: 24000,
+  //   discount: 0.1,
+  //   orders: ["Burger", "Pizza", "Salad"],
+  //   // Change code below this line
+  //   getBalance() {
+  //     return balance;
+  //   },
+  //   getDiscount() {
+  //     return discount;
+  //   },
+  //   setDiscount(value) {
+  //     discount = value;
+  //   },
+  //   getOrders() {
+  //     return orders;
+  //   },
+  //   addOrder(cost, order) {
+  //     balance -= cost - cost * discount;
+  //     orders.push(order);
+  //   },
+  //   // Change code above this line
+  // };
+  
+  // customer.setDiscount(0.15);
+  // console.log(customer.getDiscount()); // 0.15
+  // customer.addOrder(5000, "Steak");
+  // console.log(customer.getBalance()); // 19750
+  // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
